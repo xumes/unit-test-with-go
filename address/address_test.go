e412f45715f70cba3890768ed45d1f8e07ad3950
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-03-22 14:57:16 -0300

    refactor: test using test cases

--- address/address_test.go
+++ address/address_test.go
@@ -2,14 +2,36 @@ package address
 
 import "testing"
 
+type testCase struct {
+	addressToTest  string
+	expectedResult string
+}
+
 func TestAddressType(t *testing.T) {
-	mockAddress := "Sheppards run"
+	testCases := []testCase{
+		{"Sheppards Run", "Run"},
+		{"SHEPPARDS RUN", "Run"},
+		{"something else run", "Run"},
+		{"Washmill Lake Drive", "Drive"},
+		{"another drive", "Drive"},
+		{"UPPERCASE DRIVE", "Drive"},
+		{"Sam Crescent", "Crescent"},
+		{"another crescent", "Crescent"},
+		{"Main Street", "Street"},
+		{"main street", "Street"},
+		{"Xumes Lane", "Lane"},
+		{"another lane", "Lane"},
+		{"Bedford Highway", "Highway"},
+		{"another highway", "Highway"},
+		{"no name", "Invalid address type"},
+		{"45 Watter Level", "Invalid address type"},
+		{"", "Invalid address type"},
+	}
 
-	expectedAddressType := "Run"
-	currentAddressType := AddressType(mockAddress)
-
-	if currentAddressType != expectedAddressType {
-		// t.Error("Current type is different from expected")
-		t.Errorf("Received %s, Expected %s", currentAddressType, expectedAddressType)
+	for _, scenario := range testCases {
+		currentAddressType := AddressType(scenario.addressToTest)
+		if currentAddressType != scenario.expectedResult {
+			t.Errorf("Expected %s but received %s", scenario.expectedResult, currentAddressType)
+		}
 	}
 }
